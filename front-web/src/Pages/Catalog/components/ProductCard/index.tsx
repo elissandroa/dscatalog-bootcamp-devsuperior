ec884924837e4e997e0c667dfb121ffcf46525b7
Elissandro <elissandroa@hotmail.com>
2020-12-08 22:19:23 -0300

    Add product listing with backend

--- front-web/src/Pages/Catalog/components/ProductCard/index.tsx
+++ front-web/src/Pages/Catalog/components/ProductCard/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ProductPrice from '../../../../core/assets/components/ProductPrice';
-import  { ReactComponent as ProductImage} from '../../../../core/assets/images/product.svg';
+import { Product } from '../../../../core/Types/Product';
 import './styles.scss'
 
+type Props = {
+    product: Product;
+}
 
-const ProductCard = () => (
-    <div className="card-base border-radius-10 product-card">
-       <ProductImage />
+const ProductCard = ({product}:Props) => (
+     <div className="card-base border-radius-10 product-card">
+       <img src={product.imgUrl} alt={product.name} className="product-card-image"/>
        <div className="product-info">
           <h6 className="product-name">
-              Computador Desktop - Intel Core i7
+              {product.name}
           </h6>
           <div className="product-price-container">
-              <ProductPrice price="2.779,00" />
+              <ProductPrice price={product.price} />
           </div>
        </div>
     </div>
